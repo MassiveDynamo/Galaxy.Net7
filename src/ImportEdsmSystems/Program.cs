cf547247d78385d1f6ebf60919c91a2a6ea37433
MassiveDynamo <gertgregers@outlook.com>
2023-01-01 14:43:31 +0100

    Fixed ImportEdsmSystems

--- src/ImportEdsmSystems/Program.cs
+++ src/ImportEdsmSystems/Program.cs
@@ -2,13 +2,9 @@
 // & "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\SqlPackage.exe" /Sourcefile:D:\prj\Galaxy.Net7\src\eddb\bin\Release\eddb.dacpac /TargetDatabaseName:eddb /TargetServerName:"(localdb)\MSSQLLocalDB" /Action:Publish
 
 using ImportEdsmSystems;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Runtime.Intrinsics.Arm;
 using System.Text;
-using System.Text.Json;
 
 // https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/how-to-implement-a-producer-consumer-dataflow-pattern
 
@@ -30,26 +26,25 @@ if (!File.Exists(expandSystemsName))
 using StreamReader sr = new(expandSystemsName, Encoding.UTF8);
 Stopwatch w = Stopwatch.StartNew();
 
-SortedDictionary<int, EDSystemJson> list = new SortedDictionary<int, EDSystemJson>();
-
+SortedDictionary<int, EDSystemJson> list = new();
 using DataReaderExample.JsonDataReader rdr = new(expandSystemsName);
+
 while (rdr.Read())
 {
     list.Add(rdr.CurrentElement.Id, rdr.CurrentElement);
 }
 
-//SqlBulkCopy bcp = new(connectionString, SqlBulkCopyOptions.UseInternalTransaction);
-//bcp.BatchSize = 10000;
-//bcp.DestinationTableName = destinationTableName;
-//bcp.NotifyAfter = 1000 * 1000;
-//bcp.SqlRowsCopied += (sender, e) =>
-//{
-//    Console.WriteLine("Written: " + e.RowsCopied.ToString());
-//};
-//bcp.WriteToServer(rdr);
+SqlBulkCopy bcp = new(connectionString, SqlBulkCopyOptions.UseInternalTransaction);
+bcp.BatchSize = 10000;
+bcp.DestinationTableName = destinationTableName;
+bcp.NotifyAfter = 1000 * 1000;
+bcp.SqlRowsCopied += (sender, e) =>
+{
+    Console.WriteLine("Written: " + e.RowsCopied.ToString());
+};
+
+bcp.WriteToServer(rdr);
 
 w.Stop();
-// Console.WriteLine($"Lines: {lineCount}");
+Console.WriteLine($"Lines read: {list.Count}");
 Console.WriteLine($"Time elapsed: {w.Elapsed}");
-
-
